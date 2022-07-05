e4f58bd33f97a3346cecb671a0e6daa4bbc817d2
excaliburne <jeremy.faret@clarifai.com>
2022-07-06 00:12:32 +0300

    Minor updates

--- clarifai_python_sdk/modules/apps.py
+++ clarifai_python_sdk/modules/apps.py
@@ -9,17 +9,17 @@ class Apps:
     
 
     def list_all(self) -> dict:
-        """List all apps given user_id
+        """
+        List all apps given a user_id
 
         Returns:
             (dict): response object
         """
-        user_id = self.params['user_id']
-        request_type = 'get'
+        user_id  = self.params['user_id']
         endpoint = UrlHandler().build('apps', {'user_id': user_id})
 
         response = self.params['http_client'].make_request(
-            method=request_type,
+            method="get",
             endpoint=endpoint
         )
 
